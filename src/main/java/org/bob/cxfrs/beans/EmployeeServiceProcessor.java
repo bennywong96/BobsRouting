8ac31fe2c20ee167acc0841293038e59361db594
Benny <benny.wong@academytrainee.com>
2018-07-26 18:09:45 +0100

    Fixed major dependency errors

--- src/main/java/org/bob/cxfrs/beans/EmployeeServiceProcessor.java
+++ src/main/java/org/bob/cxfrs/beans/EmployeeServiceProcessor.java
@@ -2,10 +2,12 @@ package org.bob.cxfrs.beans;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.bob.cxfrs.beans.constants.Constants;
 
 public class EmployeeServiceProcessor implements Processor {
     public void process(Exchange exchange) throws Exception {
         String msg = exchange.getIn().getBody(String.class);
-        exchange.getOut().setBody("Hello World " + msg);
+        exchange.getOut().setBody(Constants.HELLO + " " + msg + "\n \n" + Constants.CONGRATS_MESSAGE +
+                Constants.FIVE_SPACES + Constants.FOOTER);
     }
 }
